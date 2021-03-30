24e287cef8ecff26fcd2ec676422867f644c8149
bazooka <bazookaforreal@gmail.com>
2021-03-29 21:07:40 -0400

    added the three necessary files

---
+++ cl_carspawner.lua
@@ -0,0 +1,54 @@
+--[[
+    My first mod to practice scripting
+]]--
+
+-- 1 is script enabled | 0 script is disabled
+SetConvar("bzk_carspawn_enable", "1")
+
+RegisterCommand('spawn_car', function(source, args)
+    -- Return if the script is disabled
+    if(GetConvarInt("bzk_carspawn_enable", 1) == 0) then return end 
+
+    local vehicleName
+
+    if(#args ~= 1)
+    then
+        -- On bad or no input use a random car from car name files
+        local car_string = LoadResourceFile("myfirstmode", "carnames.txt")
+        local car_names = {}
+        for car in car_string:gmatch("%w+") do table.insert(car_names, car) end
+
+        local rand_num = math.random(#car_names)
+        vehicleName = car_names[rand_num]
+    else
+        -- Use input user gives in chat to spawn corresponding vehicle
+        vehicleName = args[1]
+        if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName)
+        then 
+            TriggerEvent('chat:addMessage', {
+                args = {'~p~[CarSpawner] ~r~ERROR ~w~The entered model '.. vehicleName .. ' does not exist.'}
+            })
+            return
+        end
+    end
+
+    -- Wait for car model to spawn in
+    RequestModel(vehicleName)
+    while not HasModelLoaded(vehicleName) do
+        Citizen.Wait(500)
+    end
+
+    -- Spawn the car in next to the player
+    local playerPed = PlayerPedId()
+    local position = GetEntityCoords(playerPed)
+    local vehicle = CreateVehicle(vehicleName, position.x, position.y, position.z, GetEntityHeading(playerPed), true, false)
+
+    -- Put player into vehicle
+    SetPedIntoVehicle(playerPed, vehicle, -1)
+    SetEntityAsNoLongerNeeded(vehicle)
+    SetModelAsNoLongerNeeded(vehicleName)
+
+    TriggerEvent('chat:addMessage', {
+        args = {'~p~[CarSpawner] ~g~Spawned a new ' .. vehicleName .. '!!'}
+    })
+end, false)
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bazooka'
+description 'This is my server.'
+version '1.0.0'
+
+resource_type {name = 'my_resources'}
+
+files {
+    'carnames.txt'
+}
+
+client_scripts {
+    'cl_carspawner.lua'
+} 
